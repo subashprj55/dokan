74baa5695e3508ee79724b86e31d2fa845f7597e
user <user@usernoMacBook-Air.local>
2024-04-23 12:53:25 +0545

    install react icon package and also added animanation in sinin form

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,6 +1,9 @@
 'use client'
-
-import { useState } from 'react'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { FaGoogle, FaApple } from 'react-icons/fa'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const SignInPage = () => {
   const [email, setEmail] = useState('')
@@ -12,16 +15,29 @@ const SignInPage = () => {
     console.log('Signing up...')
   }
 
+  useEffect(() => {
+    // Initialize AOS
+    AOS.init({
+      duration: 1000, // Animation duration
+      easing: 'ease-in-out', // Easing option
+    })
+  }, [])
+
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+      <div
+        className="bg-white p-8 rounded-lg shadow-lg max-w-[500px] w-full "
+        data-aos="fade-up"
+      >
         <h2 className="text-3xl font-light mb-6">Create your Free Account</h2>
         <div className="mb-6 flex flex-col md:flex-row md:space-x-4">
-          <button className="bg-blue-500 text-white py-2 px-4 rounded mb-4 md:mb-0">
-            Sign up with Google
+          <button className="bg-blue-500 text-white py-2 px-4 rounded flex items-center justify-center space-x-2 mb-4 md:mb-0">
+            <FaGoogle className=" text-base" />
+            <span>Sign up with Google</span>
           </button>
-          <button className="bg-gray-500 text-white py-2 px-4 rounded mb-4 md:mb-0">
-            Sign up with Apple
+          <button className="bg-gray-500 text-white py-2 px-4 rounded flex items-center justify-center space-x-2 mb-4 md:mb-0">
+            <FaApple />
+            <span>Sign up with Apple</span>
           </button>
         </div>
 
@@ -79,9 +95,9 @@ const SignInPage = () => {
 
         <div className="mt-6 text-center">
           Already have an account?{' '}
-          <a href="#" className="text-blue-500 hover:underline">
+          <Link href="login" className="text-blue-500 hover:underline">
             Sign in here
-          </a>
+          </Link>
         </div>
       </div>
     </div>
