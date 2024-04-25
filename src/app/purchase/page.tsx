9286f0f09797e2d0ca13a3cc34bd03d87a1c3341
user <user@usernoMacBook-Air.local>
2024-04-25 07:09:31 +0545

    remove unnessary code for purchase page

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -14,7 +14,6 @@ const PurchaseItem = () => {
       <NavContainer>
         <Container>
           <PurchasePage />
-          <AddItemModal />
           <ModalPopup />
         </Container>
       </NavContainer>
@@ -39,62 +38,5 @@ const PurchasePage: React.FC = () => {
       </div>
       <ProductTable products={products} />
     </div>
-  )
-}
-
-const AddItemModal = ({ open = false }) => {
-  const [name, setName] = useState('')
-  const [quantity, setQuantity] = useState('')
-  const [price, setPrice] = useState('')
-  const [expireDate, setExpireDate] = useState('')
-
-  const handleSubmit = () => {
-    // You can handle form submission here
-    // For now, let's just log the form data
-    console.log({ name, quantity, price, expireDate })
-
-    //     // Close the modal
-    //     onClose()
-  }
-
-  return (
-    <Modal open={open}>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg">
-        <h2 className="text-2xl font-semibold mb-4">Add New Item</h2>
-        <TextField
-          label="Name"
-          variant="outlined"
-          className="mb-4 w-full"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          label="Quantity"
-          variant="outlined"
-          className="mb-4 w-full"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        />
-        <TextField
-          label="Price"
-          variant="outlined"
-          className="mb-4 w-full"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
-        <TextField
-          label="Expire Date"
-          variant="outlined"
-          className="mb-4 w-full"
-          value={expireDate}
-          onChange={(e) => setExpireDate(e.target.value)}
-        />
-        <div className="flex justify-end">
-          <Button variant="contained" color="primary" onClick={handleSubmit}>
-            Submit
-          </Button>
-        </div>
-      </div>
-    </Modal>
   )
 }
