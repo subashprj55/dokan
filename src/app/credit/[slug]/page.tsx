7460cfe47f8103e2746a066e7ddd6af2776d6503
user <user@usernoMacBook-Air.local>
2024-05-05 14:19:38 +0545

    raw code for credit page and also adding dynamic route

---
+++ src/app/credit/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>
+        this is simgle credit page where user can ditails credit transaction.
+      </h1>
+    </div>
+  )
+}
+
+export default page
