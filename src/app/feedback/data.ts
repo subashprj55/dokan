8a5746240e4ac40207a9b317315e8e125bd8c83d
user <user@usernoMacBook-Air.local>
2024-05-15 07:31:03 +0545

    update feedack page

---
+++ src/app/feedback/data.ts
@@ -0,0 +1,25 @@
+export const feedbackData = [
+  {
+    id: 0,
+    userName: 'John Doe',
+    time: '2 days ago',
+    comment:
+      'The new dashboard is amazing! I love the clean design and improved functionality.',
+    img: '/images/quickSale.jpeg',
+  },
+  {
+    id: 1,
+    userName: 'Sarah Adams',
+    time: '1 weeks ago',
+    comment: ` I have a few suggestions for improving the inventory management system. Let me know if you'd like to discuss further.`,
+    img: '/images/Sales.webp',
+  },
+  {
+    id: 2,
+    userName: 'Michael Johnson',
+    time: '3 weeks ago',
+    comment:
+      ' The new reporting features are really helpful for tracking our sales and inventory. Great work!',
+    img: '',
+  },
+]
