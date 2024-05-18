8ed1f9822355e66a6913c4474a72de9389a227ed
subashprj55 <99418785+subashprj55@users.noreply.github.com>
2024-05-19 00:45:06 +0545

    modefiy navbar with login out design (#53)
    Co-authored-by: user <user@usernoMacBook-Air.local>

--- src/components/navBar/page.tsx
+++ src/components/navBar/page.tsx
@@ -4,12 +4,31 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { usePathname } from 'next/navigation'
 import { VerticalLinks } from '../verticalNav/data'
+import { Button } from '@mui/base'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import { Avatar, Badge, Divider, Typography } from '@mui/material'
+import { TiShoppingCart } from 'react-icons/ti'
+import Container from '@/components/containder'
+import { INavMobileViewPros } from './types'
+import { IoListOutline } from 'react-icons/io5'
+import { MdOutlineClose } from 'react-icons/md'
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false)
-  const [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState<boolean>(false)
+  const [isLogIn, setIsLogIn] = useState<boolean>(true)
 
   const routerPath = usePathname()
+  const LogOutPages = ['/', '/aboutUs', '/signIn', '/login']
+
+  useEffect(() => {
+    if (LogOutPages.includes(routerPath)) {
+      setIsLogIn(false)
+    } else {
+      setIsLogIn(true)
+    }
+  }, [])
 
   useEffect(() => {
     const handleScroll = () => {
@@ -30,125 +49,184 @@ const Nav = () => {
     setIsOpen(!isOpen)
   }
 
+  const renderProfiles = () => {
+    if (!isLogIn) {
+      return (
+        <div className="hidden md:block">
+          <div className="ml-10 flex items-baseline space-x-4">
+            <Link
+              href="/"
+              className={`${routerPath === '/' ? 'text-teal-700' : ''}`}
+            >
+              <p className=" hover:text-lavender-300 px-3 py-2 rounded-md text-sm font-medium">
+                Home
+              </p>
+            </Link>
+            <Link
+              href="/aboutUs"
+              className={`${routerPath === '/aboutUs' ? 'text-teal-700' : ''}`}
+            >
+              <p className=" hover:text-lavender-300 px-3 py-2 rounded-md text-sm  font-medium">
+                About
+              </p>
+            </Link>
+            <Link
+              href="/signIn"
+              className={`${
+                routerPath === '/signIn'
+                  ? 'bg-pink-500 text-white'
+                  : 'text-pink-700'
+              } border border-pink-200 rounded-xl duration-300 hover:border-pink-600 `}
+            >
+              <p className="px-3 py-2 rounded-md text-sm  font-medium">
+                Sign In
+              </p>
+            </Link>
+            <Link href="/login">
+              <p
+                className={`${
+                  routerPath === '/login'
+                    ? 'bg-blue-600 text-white'
+                    : 'hover:text-blue-800 '
+                } px-3 py-2 text-sm font-medium rounded-xl text-blue-700 border hover:border-blue-800 duration-300`}
+              >
+                Login
+              </p>
+            </Link>
+
+            {/* Add more navigation links as needed */}
+          </div>
+        </div>
+      )
+    }
+    return <LogInNav />
+  }
+
   return (
     <>
-      <div className="fixed w-screen z-20">
+      <div className="fixed w-screen z-20 bg-gray-200 md:bg-transparent">
         <nav
-          className={`${isScrolled ? 'md:bg-gray-100' : 'bg-transparant'} ${
-            isOpen ? ' block' : ' block'
+          className={`${
+            isScrolled ? 'md:bg-gray-200' : 'md:bg-transparant'
           } transition-colors duration-500 ease-in-out `}
         >
-          <div className="max-w-7xl mx-auto px-4 py-2 sm:px-6 flex justify-between lg:px-8 md:block bg-gray-200 md:bg-transparent">
-            <div className="flex justify-between items-center">
-              <div className="flex-shrink-0">
-                <Link href={'/'}>
-                  <img
-                    className="w-[100px;]  md:w-[150px;] cursor-pointer"
-                    src="/images/logo.png"
-                    alt="Logo"
-                  />
-                </Link>
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <Link
-                    href="/"
-                    className={`${routerPath === '/' ? 'text-teal-700' : ''}`}
-                  >
-                    <p className=" hover:text-lavender-300 px-3 py-2 rounded-md text-sm font-medium">
-                      Home
-                    </p>
+          <Container>
+            <div className="py-2 flex justify-between md:block">
+              <div className="flex justify-between items-center w-full">
+                <div className="flex-shrink-0">
+                  <Link href={'/'}>
+                    <img
+                      className="w-[100px;]  md:w-[130px;] cursor-pointer"
+                      src="/images/logo.png"
+                      alt="Logo"
+                    />
                   </Link>
-                  <Link
-                    href="/aboutUs"
-                    className={`${
-                      routerPath === '/aboutUs' ? 'text-teal-700' : ''
-                    }`}
-                  >
-                    <p className=" hover:text-lavender-300 px-3 py-2 rounded-md text-sm  font-medium">
-                      About
-                    </p>
-                  </Link>
-                  <Link
-                    href="/signIn"
-                    className={`${
-                      routerPath === '/signIn'
-                        ? 'bg-pink-500 text-white'
-                        : 'text-pink-700'
-                    } border border-pink-200 rounded-xl duration-300 hover:border-pink-600 `}
-                  >
-                    <p className="px-3 py-2 rounded-md text-sm  font-medium">
-                      Sign In
-                    </p>
-                  </Link>
-                  <Link href="/login">
-                    <p
-                      className={`${
-                        routerPath === '/login'
-                          ? 'bg-blue-600 text-white'
-                          : 'hover:text-blue-800 '
-                      } px-3 py-2 text-sm font-medium rounded-xl text-blue-700 border hover:border-blue-800 duration-300`}
-                    >
-                      Login
-                    </p>
-                  </Link>
-
-                  {/* Add more navigation links as needed */}
                 </div>
+                {renderProfiles()}
+              </div>
+              <div className="-mr-2 flex md:hidden ">
+                <button
+                  onClick={toggleNavbar}
+                  type="button"
+                  className="inline-flex items-center justify-center px-2 rounded-md focus:outline-none"
+                  aria-expanded="false"
+                >
+                  {/* Icon when navbar is closed */}
+                  <IoListOutline
+                    className={`${isOpen ? 'hidden' : 'block'} text-3xl`}
+                  />
+
+                  {/* Icon when navbar is open */}
+                  <MdOutlineClose
+                    className={`${isOpen ? 'block' : 'hidden'} text-3xl`}
+                  />
+                </button>
               </div>
             </div>
-            <div className="-mr-2 flex md:hidden ">
-              <button
-                onClick={toggleNavbar}
-                type="button"
-                className=" hover:text-lavender-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {/* Icon when navbar is closed */}
-                <svg
-                  className={`${isOpen ? 'hidden' : 'block'} h-6 w-6`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16m-7 6h7"
-                  />
-                </svg>
-                {/* Icon when navbar is open */}
-                <svg
-                  className={`${isOpen ? 'block' : 'hidden'} h-6 w-6`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-          {/* Mobile menu, toggle className based on menu state */}
+          </Container>
         </nav>
       </div>
-      <div
-        className={`${
-          isOpen ? 'translate-x-0' : 'translate-x-full'
-        } md:hidden duration-500 w-full h-screen bg-white fixed z-10 top-[50px]`}
-      >
-        <div className="px-2 pb-3 space-y-1 bg-gray-100 pt-3 h-screen overflow-auto">
+      <NavMobileView
+        isNavOpen={isOpen}
+        routerPath={routerPath}
+        isLogIn={isLogIn}
+      />
+    </>
+  )
+}
+
+export default Nav
+
+const LogInNav = () => {
+  const [notificationCount, setNotificationCount] = useState(5)
+  const lowStockProducts = [
+    { id: 1, name: 'Product A', quantity: 2 },
+    { id: 2, name: 'Product B', quantity: 3 },
+    { id: 3, name: 'Product C', quantity: 1 },
+    { id: 4, name: 'Product A', quantity: 2 },
+    { id: 5, name: 'Product B', quantity: 3 },
+    { id: 6, name: 'Product C', quantity: 1 },
+  ]
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+    setNotificationCount(0)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <div className="flex space-x-4 items-center">
+      <h1 className="capitalize tracking-wide hidden md:block">
+        ABC suppliers
+      </h1>
+      <Avatar className="w-9 h-9 cursor-pointer" src="/images/user.jpg" />
+      <Button id="basic-button" onClick={handleClick}>
+        <Badge badgeContent={notificationCount} color="info">
+          <TiShoppingCart className="text-3xl" />
+        </Badge>
+      </Button>
+      <div>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              marginTop: '10px',
+              width: '350px',
+            },
+          }}
+        >
+          <h3 className="text-lg font-[500] tracking-wide text-gray-500 pl-4 mb-1">
+            Low Stock Products
+          </h3>
+
+          <Divider />
+          {lowStockProducts.map((product) => (
+            <MenuItem key={product.id}>
+              {product.name} - Quantity: {product.quantity}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
+    </div>
+  )
+}
+
+const NavMobileView = ({
+  isNavOpen,
+  routerPath,
+  isLogIn,
+}: INavMobileViewPros) => {
+  const renderContent = () => {
+    if (!isLogIn)
+      return (
+        <>
           <Link href="/">
             <p
               className={` hover:text-lavender-300 block px-3 py-2 rounded-md text-lg ${
@@ -185,22 +263,36 @@ const Nav = () => {
               Login
             </p>
           </Link>
-          {VerticalLinks.map(({ id, name, link }) => {
-            return (
-              <p
-                key={id}
-                className={` hover:text-lavender-300 block px-3 py-2 rounded-md text-lg ${
-                  routerPath === link ? 'bg-blue-500 text-white' : ''
-                }`}
-              >
-                <Link href={link}>{name}</Link>
-              </p>
-            )
-          })}
-        </div>
+        </>
+      )
+
+    return (
+      <>
+        {VerticalLinks.map(({ id, name, link }) => {
+          return (
+            <p
+              key={id}
+              className={` hover:text-lavender-300 block px-3 py-2 rounded-md text-lg ${
+                routerPath === link ? 'bg-blue-500 text-white' : ''
+              }`}
+            >
+              <Link href={link}>{name}</Link>
+            </p>
+          )
+        })}
+      </>
+    )
+  }
+
+  return (
+    <div
+      className={`${
+        isNavOpen ? 'translate-x-0' : 'translate-x-full'
+      } md:hidden duration-500 w-full h-screen bg-white fixed z-10 top-[50px]`}
+    >
+      <div className="px-2 pb-3 space-y-1 bg-gray-100 pt-3 h-screen overflow-auto">
+        {renderContent()}
       </div>
-    </>
+    </div>
   )
 }
-
-export default Nav
