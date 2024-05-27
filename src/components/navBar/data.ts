21ca458b37fb9239d332f9cedaec8c4e52f82716
subashprj55 <99418785+subashprj55@users.noreply.github.com>
2024-05-27 14:31:40 +0545

    Do 37 use state manager to store notifacation (#62)
    * update notifacation using state manager data
    
    * remove extra space
    
    ---------
    
    Co-authored-by: user <user@usernoMacBook-Air.local>

--- src/components/navBar/data.ts
+++
@@ -1,9 +0,0 @@
-export const lowStockProducts = [
-  { id: 1, name: 'Product A', quantity: 2 },
-  { id: 2, name: 'Product B', quantity: 3 },
-  { id: 3, name: 'Product C', quantity: 1 },
-  { id: 4, name: 'Product A', quantity: 2 },
-  { id: 5, name: 'Product B', quantity: 3 },
-  { id: 6, name: 'Product C', quantity: 1 },
-  { id: 7, name: 'Product C', quantity: 1 },
-]
