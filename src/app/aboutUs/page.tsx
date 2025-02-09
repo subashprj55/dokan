c6d2dacd5c9eec3cb011b2a2b28b5af73fb22722
subashprj55 <99418785+subashprj55@users.noreply.github.com>
2025-02-09 12:56:21 +0545

    Do 41 make responsive (#67)
    * make responsive video in mobile
    
    * cleaned code form about us page
    
    ---------
    
    Co-authored-by: user <user@usernoMacBook-Air.local>

--- src/app/aboutUs/page.tsx
+++ src/app/aboutUs/page.tsx
@@ -9,29 +9,7 @@ const page = () => {
     <>
       <Nav />
       <Container>
-        <div className="h-screen pt-20">
-          <div></div>
-          <div className="mt-20 p-10 shadow-md ">
-            <div className="block md:flex md:justify-between ">
-              <div className="mb-5 md:mb-0">
-                <iframe
-                  src="https://player.vdocipher.com/v2/?otp=20160313versASE323Xka4FBiRbSHMzus9q8DiuWcG14OCvcBRlbdTWEdzIg8bkR&playbackInfo=eyJ2aWRlb0lkIjoiY2FlZjFkZmQzOWQ1N2E0MGMwY2JmMDVmOGI4NmI4MDcifQ=="
-                  className=" h-auto w-[540px;] max-w-full"
-                  allowFullScreen
-                  allow="encrypted-media"
-                ></iframe>
-              </div>
-              <div>
-                <iframe
-                  src="https://player.vdocipher.com/v2/?otp=20160313versASE323P3PHwd1NcKKUiEkOYxFCRVTTeQW5dfWmC1hKYw4zHxDg74&playbackInfo=eyJ2aWRlb0lkIjoiNDU5OTY1YTM2ZDVhNDBkOGIzN2JhNmUwYzE5ZGRiMjMifQ=="
-                  className="h-auto w-[540px;] max-w-full"
-                  allowFullScreen
-                  allow="encrypted-media"
-                ></iframe>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="h-screen pt-20"></div>
         <Footer />
       </Container>
     </>
