d3377faba32179dd19eea32431b336cc7acc8c8d
subashprj55 <subashprj55@gmail.com>
2025-02-09 22:46:11 +0545

    taked aos config in useEffect

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,19 +1,22 @@
 'use client'
-
+import { ReactNode, useEffect } from 'react'
+// for api calling
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactNode } from 'react'
 // aos scroll animation package
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+// for notification
 import { Toaster } from 'react-hot-toast'
 
 export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = new QueryClient()
 
-  AOS.init({
-    duration: 1000,
-    easing: 'ease-in-out',
-  })
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+    })
+  }, [])
 
   return (
     <>
